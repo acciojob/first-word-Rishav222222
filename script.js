32baa2478ccccdec05a48e09019e14eaf49853b8
Rishav222222 <160915777+Rishav222222@users.noreply.github.com>
2024-06-26 11:40:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 function firstWord(s) {
-  // your code here
+    // Trim leading spaces to handle cases where the string starts with spaces
+    s = s.trimStart();
+
+    // If the string is empty after trimming, return an empty string
+    if (s === "") {
+        return "";
+    }
+
+    // Find the index of the first space
+    const firstSpaceIndex = s.indexOf(' ');
+
+    // If there is no space, return the entire string
+    if (firstSpaceIndex === -1) {
+        return s;
+    }
+
+    // Return the substring up to the first space
+    return s.substring(0, firstSpaceIndex);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
-alert(firstWord(s));
+alert(firstWord(s)); 
+
+
+
